d27e02c05af747ad48c02a517649d511eab7872d
Anthony Edelstein <asedelstein@gmail.com>
2018-02-16 18:47:57 -0800

    prelime submit for hw

--- game.js
+++ game.js
@@ -3,70 +3,85 @@
 
 
 
-var WineChoices = ["Chianti", "Champagne", "Bordeaux", "Meritage", "Malbec", " Pinot Noir", "Sauvignon Blanc"];
+var wineChoices = ["Chianti","Champagne","Bordeaux","Meritage","Malbec","Chardonnay"];
 //variable of the alaphabet for letter choices goes here -Array Include wins, losses, and guesses
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
-var guessesRemaining = 14;
 var wins = 0;
 var loss = 0;
-var underScore = [];
-var randomWine;
+var underScore ="";
 var wrongLetter = [];
-var userGuesses = [];
 var userScore = [];
-var winCounter = [];
-var lossCounter = [];
+var winCounter = 0;
+var guessesLeft = 10;
+var userGuesses = [];
+var lossesCounter = 0;
+var letters = [];
 
-//Show alphabet for letter selection
-var buttons = function () {
-    myButtons = document.getElementById("buttons");
-    letters = document.createElement('ul');
- 
-    for (var i = 0; i < alphabet.length; i++) {
-      letters.id = "alphabet";
-      list = document.createElement('li');
-      list.id = 'letter';
-      list.innerHTML = alphabet[i];
-      check();
-      myButtons.appendChild(letters);
-      letters.appendChild(list);
+//Start Game function???
+
+//pick random word from wineChoice array
+    randomWine = wineChoices[Math.floor(Math.random() * wineChoices.length)];
+    console.log(randomWine.length);
+    for(var i = 0; i < randomWine.length; i++);{
+            
+}
+
+    for(var i = 0; i < randomWine.length; i++){
+    underScore +="_ ";
+}
+
+  //Underscore blanks will appear on screen include number of guesses, wrong guesses, and guesses left. 
+    function displayNumberOfGuesses(number) {
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
+    guessesLeft = 10;
     }
-  }
+    document.getElementById("word-blanks").innerHTML = underScore;
 
-//Start Game function
-  
 
-function startGame(){
-    randomWord = WineChoices[Math.floor(Math.random() * WineChoices.length)];
-    console.log(randomWord);{
-    underScore.push('')
-    }
-    for(var i = 0; i < randomWine.length; i++);
+    //Keyboard interactive-- cannot get full functionality. It
+    var usedChars = [];
+    document.onkeyup = function (event) {
+    var key_press = String.randomWine(event.keyCode);
+    console.log(key_press);
+    usedChars.push(key_press)
+    document.getElementById("wrong-guesses").innerHTML = usedChars.toString();
 
-    
-    }
-//Underscore blanks will appear on screen
+    if(randomWine.indexof(userGuesses) > -1){
+        for(var i = 0; i < randomWine.length; i++){
+           underScore.push(' _');
+        }
+        if(randomWine[i] === userGuesses);{
+           underScore[i] = userGuesses;
+           winCounter++;
+           winLose();
+        } 
+       }
 
-function makeUnderScore()
-   if (WineChoices.length === 0)
-   {
-       var underScoreBlack = rightWordtxt.txtContent = [];
+       else{
+           wrongLetter.push(userGuesses);
+           guessesLeft --;
+           console.log(wrongLetter);
+       }
    }
-document.getElementById("word-blanks").innerHTML = underScore;
-console.log(underScore)
-//reset
-wrongLetter = [];
-guessesRemaining = 14;
 
-//Affects the HTML code
-document.getElementById("guesses-left").textContent = guessesRemaining;{
+        /* userGuesses = event.key.toUpperCase;
+     for (i = 0; i < randomWine.length; i++)
+     if (userGuesses === randomWine[i])
+     document.getElementById("answer").innerHTML = letters.join(" _");
+     console.log(userGuesses);
+    }
+    */
+
+//win Lose alerts for letter choices
+function winLose(){
+    if(winCounter === randomWine.length){
+        alert("Winner!");
+         
+    }
+    else if(guessesLeft === 0){
+        alert("You're Drunk Go Home!");
+        
+    }
 
 }
 
-//reset
-
-//Affects the HTML code
-document.getElementById("guesses-left").textContent = guessesRemaining;{
-
-}
-    //Checking to see if the letter exists inside of the word choice.
